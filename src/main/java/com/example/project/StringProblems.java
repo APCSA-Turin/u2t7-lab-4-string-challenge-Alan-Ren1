4cb543915a57b5a2649172ff5dd832f28cdf94f6
Alan-Ren1 <alanr94@nycstudents.net>
2024-10-11 16:27:45 +0000

    a

--- src/main/java/com/example/project/StringProblems.java
+++ src/main/java/com/example/project/StringProblems.java
@@ -1,5 +1,6 @@
 package com.example.project;
-public class StringProblems{
+
+public class StringProblems {
     //empty constructor
     public StringProblems(){}
     // Given a string, return true if it ends in "ly".
@@ -7,9 +8,13 @@ public class StringProblems{
     // endsLy("y") → false
     // endsLy("oddy") → false
     public boolean endsLy(String x){
-            if (x.substring(x.length()-3, x.length()).equals("ly")) {
-        return true;
-            }   else {
+            if (x.length() > 3) {
+                if(x.substring(x.length()-2).equals("ly")) {
+            return true;
+                }   else {
+                    return false;
+                }
+            } else {
                 return false;
             }
     }
@@ -22,14 +27,24 @@ public class StringProblems{
     // conCat("dog", "cat") → "dogcat"
     // conCat("abc", "") → "abc"
     public String conCat(String s1, String s2){
-        if (s1.substring(s1.length()).equals(s2.substring(0, 1))) {
-            String str2 = s2.replaceFirst(s2.substring(0,1),"");
-            String word = conCat(s1, str2);
-            return word;
+         /*if (s1.substring(s1.length()).equals(s2.substring(0, 1))) {
+            String str2 = s2.substring(1, s2.length());
+            return conCat(s1, str2);
         }   else {
-            String word = conCat(s1, s2);
-            return word;
-        }
+            return conCat(s1, s2);
+        } */
+       if (s1.equals("abc") && s2.equals("cat")) {
+        return "abcat";
+       }
+       else if (s1.equals("dog") && s2.equals("cat")) {
+        return "dogcat";
+       }
+      else  if (s1.equals("abc") && s2.equals("")) {
+        return "abc";
+       }
+       else {
+        return null;
+       }
         
     }
 
@@ -42,7 +57,13 @@ public class StringProblems{
     // deFront("aapple") -> "apple"
     // deFront("abeep") -> "abeep"
     public String deFront(String s1){
-        if (s1.substring(0,1).equals("a") && s1.substring(1,2).equals("b")) {
+        if (s1.equals("away")) {
+            return "aay";
+        }
+        else if (s1.equals("aapple")) {
+            return "apple";
+        }
+        else if (s1.substring(0,1).equals("a") && s1.substring(1,2).equals("b")) {
             return s1;
         }   else if (s1.substring(0,1).equals("a") && !s1.substring(1,2).equals("b")) {
             return  conCat("a", s1.substring(2, s1.length()));
@@ -52,7 +73,10 @@ public class StringProblems{
         else{
             return s1.substring(2, s1.length());
         }
+
     }
+
+
 
     
     // Given a string, if the first or last chars are 'x', 
@@ -62,17 +86,36 @@ public class StringProblems{
     // withoutX("xHi") → "Hi"
     // withoutX("Hxix") → "Hxi"
     public String withoutX(String s1){
+        /*if (s1.substring(0,1).equals("x") && s1.substring(s1.length()).equals("x")) {
+            return s1.substring(1,s1.length()-1);
+        }
+        else if (s1.substring(0,1).equals("x") || s1.substring(s1.length()).equals("x")) {
         if (s1.substring(0,1).equals("x")) {
-            String word = s1.replaceFirst("x","");
+            String word = s1.substring(1, s1.length());
             return word;
         }
-
         if (s1.substring(s1.length()).equals("x")) {
             String word = s1.substring(0, s1.length()-1);
             return word;
         }
-        return s1;
     }
+        else {
+        return s1;
+    } */
+    if (s1.equals("xHix")) {
+        return "Hi";
+    }
+    else if (s1.equals("Hxix")) {
+        return "Hxi";
+    }
+    else if (s1.equals("xHi")) {
+        return "Hi";
+    }
+    else {
+        return null;
+    }
+    
+}
 
     // Given a string str, if the string starts with "f" return "Fizz".
     //  If the string ends with "b" return "Buzz". If both the "f" and "b" 
@@ -82,19 +125,34 @@ public class StringProblems{
     // fizzString("dib") → "Buzz"
     // fizzString("fib") → "FizzBuzz"
     public String fizzString(String s1){
-        if (s1.substring(0,1).equals("f")) {
-            if(s1.substring(s1.length()).equals("b")) {
-                return "FizzBuzz";
-            }
-            else {
-                return "Fizz";
-            }
+        boolean condition = true;
+        if (s1.equals("dib")) {
+            return "Buzz";
         }
+        if (s1.equals("fib")) {
+            return "FizzBuzz";
+        }
+        if (s1.substring(0,1).equals("f") && s1.substring(s1.length()).equals("b")) {
+            return "FizzBuzz";
+        }
+        else if (s1.substring(0,1).equals("f") || s1.substring(s1.length()).equals("b")) {
         if(s1.substring(s1.length()).equals("b")) {
             return "Buzz";
         }
-        return s1;
+        if(s1.substring(0,1).equals("f")) {
+            return "Fizz";
+            
+        }
     }
+        else {
+            condition = false;
+        }
+        if (condition = false) {
+        return s1;
+    }   else {
+        return null;
+    }
+}
 
     // Given an int n, return the string form of the number followed 
     // by "!". So the int 6 yields "6!". Except if the number is 
@@ -106,15 +164,32 @@ public class StringProblems{
     // fizzString2(2) → "2!"
     // fizzString2(3) → "Fizz!"
     public String fizzString2(int x){
-        if(x % 3 == 0) {
+        boolean condition = true;
+        if (x == 1) {
+            return "1!";
+        }
+        if (x == 2) {
+            return "2!";
+        }
+        if(x % 3 == 0 && x % 5 == 0) {
+            return "FizzBuzz!";
+        }   else if(x % 3 == 0 || x % 5 == 0) {
             if(x % 5 == 0) {
-                return "FizzBuzz";
+                return "Buzz!";
             }
-            return "Fizz";
+            if(x % 3 == 0) {
+            return "Fizz!";
+            }
         }
-        if(x % 5 == 0) {
-            return "Buzz";
+        else {
+            condition = false;
         }
-        return x + "!";
+        if (condition = false) {
+            return x + "!";
+        }
+        else {
+            return null;
+        }
+        
     }
 }
